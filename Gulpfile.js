f9c66bd0c2b216cf44e2aa0936393e8b8c9f3d9e
Stephanie Marx <stephanie.n.marx@gmail.com>
2016-12-07 22:48:00 -0600

    Initial project setup, including gulp task for compiling sass

---
+++ Gulpfile.js
@@ -0,0 +1,15 @@
+// from http://ryanchristiani.com/getting-started-with-gulp-and-sass/
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('sass/**/*.scss',['styles']);
+});
